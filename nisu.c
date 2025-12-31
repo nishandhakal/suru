043b81e67c43732741b5c99c10d352d94eecda6a
Nishan Dhakal <nishancsit25@oic.edu.np>
2025-12-31 07:47:53 +0545

    first commit

--- nisu.c
+++ nisu.c
@@ -2,13 +2,17 @@
 #include<conio.h>
 int main(){
 
-	int i,mark[5];
-	printf("Enter a mark of 5 students");
-	scanf("%d %d %d %d %d",&mark[0],&mark[1],&mark[2],&mark[3],&mark[4]);
+	int i,size[5];
+	printf("Enter the sizes of 5 students:\n");
+	scanf("%d %d %d %d %d",&size[0],&size[1],&size[2],&size[3],&size[4]);
 	for(i=0; i<5;i++){
 		
-	printf("mark of %d std= %d",i,mark[i]);
-	printf("\n",i,mark[5]);
+	printf("size of %d std= %d",i,size[i]);
+	printf("\n",i,size[5]);
+	}
+	for(i=5; i>=1;i--){
+		printf("size of %d std= %d",i,size[i-1]);
+		printf("\n",i,size[5]);
 	}
 
 	getch();
